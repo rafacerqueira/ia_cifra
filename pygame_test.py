f87018291a3b25c0f89f8f3ba74d2aa81e8ad8f6
Rafael Cerqueira <up201910200@fe.up.pt>
2023-03-08 12:05:10 +0000

    3x3 board, and white pieces start on top

--- pygame_test.py
+++ pygame_test.py
@@ -13,10 +13,12 @@ pygame.init()
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
 # definir as cores do tabuleiro
-BLUE = (0, 0, 255)
+DARK_BLUE = (0, 50, 130)
 WHITE = (255, 255, 255)
-RED = (255, 0, 0)
+GREY = (153, 153, 153)
 YELLOW = (255, 255, 0)
+LIGHT_BLUE = (115, 169, 255)
+LIGHT_DARK = (204, 209, 217)
 
 
 #lista de pecas
@@ -25,43 +27,54 @@ pieces = []
 # criar uma lista de 24 quadrados
 squares = []
 
-for i in range(12):
+for i in range(4):
     squares.append({'color': WHITE, 'rect': pygame.Rect(0, 0, 100, 100)})
-    squares.append({'color': BLUE, 'rect': pygame.Rect(0, 0, 100, 100)})
+    squares.append({'color': DARK_BLUE, 'rect': pygame.Rect(0, 0, 100, 100)})
     
 
 
 random.shuffle(squares)
 
-squares.insert(11,{'color': RED, 'rect': pygame.Rect(0, 0, 100, 100)})
-squares[11]['color'] = RED
+squares.insert(3,{'color': GREY, 'rect': pygame.Rect(0, 0, 100, 100)})
+squares[3]['color'] = GREY
 
 # desenhar um tabuleiro de 5x5 com quadrados aleatórios
 square_size = 100 # cada quadrado terá 100 pixels de largura e altura
 square_spacing = 2
 x = 50 # posição x do canto superior esquerdo do primeiro quadrado
 y = 50 # posição y do canto superior esquerdo do primeiro quadrado
-for row in range(5): # loop pelas 5 linhas do tabuleiro
-    for col in range(5): # loop pelas 5 colunas do tabuleiro
+for row in range(3): # loop pelas 5 linhas do tabuleiro
+    for col in range(3): # loop pelas 5 colunas do tabuleiro
         
-        square = squares[row * 5 + col -1 ]
+        square = squares[row * 3 + col -1 ]
         # atualizar a posição do quadrado para a posição atual do loop
         square['rect'].x = x + col * (square_size + square_spacing)
         square['rect'].y = y + row * (square_size + square_spacing)
         # desenhar o quadrado atual
         pygame.draw.rect(window, square['color'], square['rect'])
         
-        if row == 0 or row == 4:
+        if row == 0:
             center_x = square['rect'].x + square_size // 2
             center_y = square['rect'].y + square_size // 2
             
             # desenhar um círculo amarelo no centro do quadrado atual
-            pygame.draw.circle(window, YELLOW, (center_x, center_y), square_size // 2 - 5)
+            pygame.draw.circle(window, LIGHT_DARK, (center_x, center_y), square_size // 2 - 5)
             
-            circle = {'color': YELLOW, 'center': (center_x, center_y)}
+            circle = {'color': LIGHT_DARK, 'center': (center_x, center_y)}
             
             pieces.append(circle)
-print(pieces[6])
+            
+        if row == 2:
+            center_x = square['rect'].x + square_size // 2
+            center_y = square['rect'].y + square_size // 2
+            
+            # desenhar um círculo amarelo no centro do quadrado atual
+            pygame.draw.circle(window, LIGHT_BLUE, (center_x, center_y), square_size // 2 - 5)
+            
+            circle = {'color': LIGHT_BLUE, 'center': (center_x, center_y)}
+            
+            pieces.append(circle)
+
 # atualizar a tela
 pygame.display.update()
 
